32cadb1a497253390d9e0bbb72421f3d115598e3
yuanguangji <1351527793@163.com>
2026-02-25 00:25:41 +0800

    fix: 样式调整

--- src/pages/user/login/index.tsx
+++ src/pages/user/login/index.tsx
@@ -12,15 +12,18 @@ import {
   ProFormCheckbox,
   ProFormText,
 } from '@ant-design/pro-components';
-import { Helmet, useModel } from '@umijs/max';
+import { Helmet, history, useModel } from '@umijs/max';
 import { Alert, App, Tabs } from 'antd';
 import { createStyles } from 'antd-style';
+import { parse } from 'query-string';
 import React, { useState } from 'react';
+import { useAliveController } from 'react-activation';
 import { flushSync } from 'react-dom';
 import { Footer } from '@/components';
 import { getFakeCaptcha } from '@/services/ant-design-pro/login';
 import { login } from '@/services/web/login';
 import { pwd } from '@/utils/Encrypt';
+import { Token, User } from '@/utils/Web';
 import Settings from '../../../../config/defaultSettings';
 
 const useStyles = createStyles(({ token }) => {
@@ -94,9 +97,11 @@ const LoginMessage: React.FC<{
 };
 const Login: React.FC = () => {
   const [userLoginState, setUserLoginState] = useState<API.LoginResult>({});
-  const { initialState, setInitialState } = useModel('@@initialState');
+  const { refresh, initialState, setInitialState } = useModel('@@initialState');
   const { styles } = useStyles();
   const { message } = App.useApp();
+  // const { clear } = useAliveController();
+
   const fetchUserInfo = async () => {
     const userInfo = await initialState?.fetchUserInfo?.();
     if (userInfo) {
@@ -109,18 +114,46 @@ const Login: React.FC = () => {
     }
   };
   const handleSubmit = async (values: API.LoginParams) => {
-    console.log(values, 'values');
     try {
       // 登录
       login({ ...values, password: pwd.encrypt(`${values.password}`) }).then(
         async (res) => {
           // 解析远程数据
-          console.log(res);
+          const remoteUser = {
+            ...res,
+            roles: res.attributes.roleCodes,
+            permissions: res.attributes.permissions,
+          };
+
+          console.log(initialState, 'initialState');
+          console.log(res.info, 'res.info');
+          if (res.info) {
+            flushSync(() => {
+              setInitialState((s) => ({
+                ...s,
+                currentUser: res.info,
+              }));
+            });
+          }
+          console.log(initialState, 'initialState2');
+
+          // 清空现有的tab缓存
+          // clear();
+          // 缓存用户信息
+          User.set(JSON.stringify(remoteUser));
+          // 缓存token
+          Token.set(remoteUser.access_token);
+          const { redirect } = parse(history.location.search) as {
+            redirect: string;
+          };
+          message.success('登录成功');
+          history.replace(redirect || '/');
+          // 则刷新数据
+          await refresh();
         },
       );
     } catch (error) {
       const defaultLoginFailureMessage = '登录失败，请重试！';
-      console.log(error);
       message.error(defaultLoginFailureMessage);
     }
   };
