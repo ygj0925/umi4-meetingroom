fb0f3d0dec1a73ac149f16fd0bdc31c743cb09ad
yuanguangji <yuanguangji@3sbio.com>
2026-03-12 14:28:37 +0800

    fix: 暂存

--- src/layouts/BaseLayouts.tsx
+++ src/layouts/BaseLayouts.tsx
@@ -1,6 +1,11 @@
 import {
   DashboardOutlined,
   FormOutlined,
+  GithubFilled,
+  InfoCircleFilled,
+  PlusCircleFilled,
+  QuestionCircleFilled,
+  SearchOutlined,
   SmileOutlined,
   TableOutlined,
   UserOutlined,
@@ -16,14 +21,13 @@ import {
   SettingDrawer,
   WaterMark,
 } from '@ant-design/pro-components';
-import { css } from '@emotion/css';
 import { history, Link, Outlet, useLocation, useModel } from '@umijs/max';
-import { theme } from 'antd';
+import { Input, theme } from 'antd';
 import React, { type FC, useEffect, useMemo } from 'react';
 import defaultSettings from '@/../config/defaultSettings';
 import routesConfig from '@/../config/routes';
 import Footer from '@/components/Footer';
-import { isLogin } from '@/utils/Web';
+import { isLogin, LayoutSetting } from '@/utils/Web';
 
 /**
  * icon 映射
@@ -82,11 +86,54 @@ export type BaseLayoutProps = {
   breadcrumbNameMap: Record<string, MenuDataItem>;
 } & ProLayoutProps;
 
+const SearchInput = () => {
+  const { token } = theme.useToken();
+  return (
+    <div
+      key="SearchOutlined"
+      aria-hidden
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        color: token.colorPrimary,
+        marginInlineEnd: 24,
+      }}
+      onMouseDown={(e) => {
+        e.stopPropagation();
+        e.preventDefault();
+      }}
+    >
+      <Input
+        style={{
+          borderRadius: 4,
+          marginInlineEnd: 12,
+          backgroundColor: token.colorBgTextHover,
+        }}
+        prefix={
+          <SearchOutlined
+            style={{
+              color: token.colorPrimary,
+            }}
+          />
+        }
+        placeholder="搜索方案"
+        variant="borderless"
+      />
+      <PlusCircleFilled
+        style={{
+          color: token.colorPrimary,
+          fontSize: 24,
+        }}
+      />
+    </div>
+  );
+};
+
 /**
  * BaseLayout
  */
 
-const BaseLayout: FC<BaseLayoutProps> = (props) => {
+const BaseLayout: FC<BaseLayoutProps> = () => {
   const location = useLocation();
 
   const { initialState, setInitialState } = useModel('@@initialState');
@@ -146,6 +193,57 @@ const BaseLayout: FC<BaseLayoutProps> = (props) => {
       title={defaultSettings.title}
       logo={defaultSettings.logo}
       location={location}
+      appList={[
+        {
+          icon: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+          title: 'Ant Design',
+          desc: '杭州市较知名的 UI 设计语言',
+          url: 'https://ant.design',
+        },
+        {
+          icon: 'https://gw.alipayobjects.com/zos/antfincdn/FLrTNDvlna/antv.png',
+          title: 'AntV',
+          desc: '蚂蚁集团全新一代数据可视化解决方案',
+          url: 'https://antv.vision/',
+          target: '_blank',
+        },
+        {
+          icon: 'https://gw.alipayobjects.com/zos/antfincdn/upvrAjAPQX/Logo_Tech%252520UI.svg',
+          title: 'Pro Components',
+          desc: '专业级 UI 组件库',
+          url: 'https://procomponents.ant.design/',
+        },
+        {
+          icon: 'https://img.alicdn.com/tfs/TB1zomHwxv1gK0jSZFFXXb0sXXa-200-200.png',
+          title: 'umi',
+          desc: '插件化的企业级前端应用框架。',
+          url: 'https://umijs.org/zh-CN/docs',
+        },
+        {
+          icon: 'https://gw.alipayobjects.com/zos/bmw-prod/8a74c1d3-16f3-4719-be63-15e467a68a24/km0cv8vn_w500_h500.png',
+          title: 'qiankun',
+          desc: '可能是你见过最完善的微前端解决方案🧐',
+          url: 'https://qiankun.umijs.org/',
+        },
+        {
+          icon: 'https://gw.alipayobjects.com/zos/rmsportal/XuVpGqBFxXplzvLjJBZB.svg',
+          title: '语雀',
+          desc: '知识创作与分享工具',
+          url: 'https://www.yuque.com/',
+        },
+        {
+          icon: 'https://gw.alipayobjects.com/zos/rmsportal/LFooOLwmxGLsltmUjTAP.svg',
+          title: 'Kitchen ',
+          desc: 'Sketch 工具集',
+          url: 'https://kitchen.alipay.com/',
+        },
+        {
+          icon: 'https://gw.alipayobjects.com/zos/bmw-prod/d3e3eb39-1cd7-4aa5-827c-877deced6b7e/lalxt4g3_w256_h256.png',
+          title: 'dumi',
+          desc: '为组件开发场景而生的文档工具',
+          url: 'https://d.umijs.org/zh-CN',
+        },
+      ]}
       /**
        * 菜单
        */
@@ -158,7 +256,9 @@ const BaseLayout: FC<BaseLayoutProps> = (props) => {
        * mix = 顶部 + 左侧
        */
 
-      layout="mix"
+      navTheme="light"
+      layout={defaultSettings.layout as 'top' | 'side' | 'mix'}
+      contentWidth={defaultSettings.contentWidth as 'Fluid' | 'Fixed'}
       /**
        * 顶部菜单
        */
@@ -182,7 +282,17 @@ const BaseLayout: FC<BaseLayoutProps> = (props) => {
        * 顶部 actions
        */
 
-      actionsRender={() => []}
+      actionsRender={(props) => {
+        if (props.isMobile) return [];
+        return [
+          props.layout !== 'side' ? (
+            <SearchInput key="SearchInput" />
+          ) : undefined,
+          <InfoCircleFilled key="InfoCircleFilled" />,
+          <QuestionCircleFilled key="QuestionCircleFilled" />,
+          <GithubFilled key="GithubFilled" />,
+        ];
+      }}
       /**
        * 头像
        */
@@ -196,14 +306,18 @@ const BaseLayout: FC<BaseLayoutProps> = (props) => {
        * footer
        */
 
+      onMenuHeaderClick={(e) => {
+        e?.stopPropagation?.();
+        history.push('/');
+      }}
       footerRender={() => <Footer />}
       /**
        * 侧边栏
        */
 
-      fixSiderbar
-      fixedHeader
-      siderWidth={220}
+      fixedHeader={defaultSettings.fixedHeader}
+      fixSiderbar={defaultSettings.fixSiderbar}
+      siderWidth={256}
       /**
        * breadcrumb
        */
@@ -226,12 +340,14 @@ const BaseLayout: FC<BaseLayoutProps> = (props) => {
       <SettingDrawer
         pathname={location.pathname}
         enableDarkTheme
-        settings={initialState?.settings}
-        onSettingChange={(settings) => {
-          setInitialState({
-            ...initialState,
-            settings,
-          });
+        hideHintAlert
+        hideCopyButton
+        disableUrlParams
+        settings={(initialState as any)?.settings}
+        onSettingChange={(changeSetting) => {
+          const nextSettings = { ...initialState?.settings, ...changeSetting };
+          setInitialState({ ...initialState, settings: nextSettings });
+          LayoutSetting.set(nextSettings);
         }}
       />
     </ProLayout>
